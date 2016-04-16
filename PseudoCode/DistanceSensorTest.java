1140c320c0760c7d2b0ab43109940f4d2eaef677
Bridget <bridgetj18@users.noreply.github.com>
2016-04-16 15:56:56 -0700

    Added files via upload

--- PseudoCode/DistanceSensorTest.java
+++ PseudoCode/DistanceSensorTest.java
@@ -8,6 +8,14 @@ import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 public class DistanceSensorTest extends OpMode{
 
     OpticalDistanceSensor floorIR;
+
+    boolean bluedetected = false;
+    boolean redDetected = false;
+    boolean whiteDetected = false;
+    boolean greyDetected = false;
+
+    String colorDetected = "no color detected";
+
     @Override
     public void init(){
         floorIR = hardwareMap.opticalDistanceSensor.get("ir");
@@ -20,5 +28,36 @@ public class DistanceSensorTest extends OpMode{
 
         double rawValue = floorIR.getLightDetectedRaw();
         telemetry.addData("RawLight", rawValue);
+
+        if ((reflectance >= 125)||(reflectance <= 153)){
+            bluedetected = true;
+            colorDetected = "blue";
+        } else {
+            bluedetected = false;
+        }
+
+        if ((reflectance >= 532)||(reflectance <= 632)){
+            redDetected = true;
+            colorDetected = "red";
+
+        } else {
+            redDetected = false;
+        }
+
+        if ((reflectance >= 700)||(reflectance <= 750)){
+            whiteDetected = true;
+            colorDetected = "white";
+        } else {
+            whiteDetected = false;
+        }
+
+        if ((reflectance >= 108)||(reflectance <= 125)){
+            greyDetected = true;
+            colorDetected = "tile";
+        } else {
+            greyDetected = false;
+        }
+
+        telemetry.addData("Color Detected:", colorDetected);
     }
 }
